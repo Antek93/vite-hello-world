a3dbcf22fc12350daaf96bc8ed5c7688de7c95ba
Antek93 <antonio.savino1993@gmail.com>
2023-02-01 17:20:50 +0100

    esercizio completato + bonus

---
+++ src/components/AppHero.vue
@@ -0,0 +1,23 @@
+<script setup>
+</script>
+
+<template>
+    <div class="photo">
+        <div>Lorem ipsum dolor sit amet consectetur adipisicing elit.Tenetur doloribus magni praesentium. Officia atque neque illum dicta, impedit provident?</div>
+        <img id="windows" src="https://articles-images.sftcdn.net/wp-content/uploads/sites/4/2014/04/windows_xp_bliss-wide1.jpg" alt="ko">
+    </div>
+</template>
+
+<style scoped>
+.photo {
+  position: absolute;
+  width: 30%;
+  height: 30%;
+  transform: translate(-50%,-50%);
+  bottom: 0%;
+  left:50%;
+}
+#windows{
+    width: 100%;
+}
+</style>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,13 @@
+<script setup>
+</script>
+
+<template>
+    <h1>La mia prima app con Vite!</h1>
+</template>
+
+
+<style scoped>
+h1 {
+   color: yellowgreen;
+}
+</style>
