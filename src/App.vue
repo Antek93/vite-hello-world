b0ff9dd02202c75d341b48d0d00068cf011efa3d
Antek93 <antonio.savino1993@gmail.com>
2023-02-02 10:41:07 +0100

    aggiustato script senza setup

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,27 @@
 <script>
-import AppTitle from './components/AppTitle.vue';
+import AppTitle from './components/AppTitle.vue'; //importare ogni singolo componente che creiamo tramite queste linee di codice
 import AppHero from './components/AppHero.vue';
 
-export default {
-    name: 'App',
+export default { //questa formula serve ad importante i componenti ed utilizzarli nel mio file
+    name: 'App', //Name richiama il nome del file
 
-    data () {
-        return {
+    data () { //In data utilizziamo e richiamiamo gli elementi key che dichiariamo
+        return { 
             msg: 'Ciao',
             count: 0,
         }
 
     },
-    components: {
+    components: { //Qui richiamamo il nome dei file che abbiamo importato
       AppTitle,
       AppHero
     }
 }
 </script>
 
-<template>
-    <div>
-      <AppTitle />
+<template> 
+    <div> 
+      <AppTitle /> 
     </div>
     <div>
       <AppHero />
